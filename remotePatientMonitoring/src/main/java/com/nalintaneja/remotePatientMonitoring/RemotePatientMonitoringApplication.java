eb9badd7844da2fca59832dc10de24e0a016ebae
NALIN TANEJA <nalintaneja123@gmail.com>
2026-07-23 23:39:01 +0530

    creating first commit

---
+++ remotePatientMonitoring/src/main/java/com/nalintaneja/remotePatientMonitoring/RemotePatientMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.nalintaneja.remotePatientMonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemotePatientMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemotePatientMonitoringApplication.class, args);
+	}
+
+}
