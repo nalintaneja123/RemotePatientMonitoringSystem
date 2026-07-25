cee0b253deea8eefd5e7aae849abf68a38d1fb1a
NALIN TANEJA <nalintaneja123@gmail.com>
2026-07-25 19:55:23 +0530

    Creating Alerts Microservice

---
+++ Alerts/src/test/java/com/example/Alerts/AlertsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Alerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
